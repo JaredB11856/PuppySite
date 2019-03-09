9926943779112a14e1dd5a693014f889554b06d9
Jared Brown <Jb_90210@hotmail.com>
2019-03-09 16:34:56 -0700

    Updated seed for puppy, Put puppt info into a row

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Litter.create!(
 puts '1 Litter created'
 
 Puppy.create!(
-    about: "This is our littlest puppy. He's such a fiesty ",
+    about: "This is the story of our puppy Chad, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     image_one: "https://www.freepngimg.com/thumb/puppy/33435-9-siberian-husky-puppy-photos.png",
     id: 1,
     litter_id: 1
