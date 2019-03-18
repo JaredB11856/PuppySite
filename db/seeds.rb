4479527c6b4c94abccf9f4e14ef4a2f63705e37e
Jared Brown <Jb_90210@hotmail.com>
2019-03-17 22:11:12 -0600

    stlyed home, our dogs, litters & puppies

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 Mother.create!(
-    about: "about",
+    about: "This is the story of our Mother Chaddette, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "https://www.freepngimg.com/thumb/dog/10-dog-png-image.png"
   )
 
 puts '1 Mother created'
 
 Litter.create!(
-    about: "This is our littlest Litter",
+    about: "This is the story of our Litter from Chaddette, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     main_image: "https://www.freepngimg.com/thumb/dog/15-dog-png-image-picture-download-dogs.png",
     mother_id: 1
   )
