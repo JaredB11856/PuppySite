e8b05136fb4b65d635afb7cf4a9a92b72a0bdca4
Jared Brown <Jb_90210@hotmail.com>
2019-06-24 01:30:12 -0600

    Styled form pages & Changed background to visitor only pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController 
+  layout "visitor", :only => [ :home, :our_dogs ]
 
   def home
     @puppies = Puppy.all
+    render(:layout => "layouts/application")
   end
 
   def our_dogs
     @mothers = Mother.all
+    render(:layout => "layouts/application")
   end
 
 end
